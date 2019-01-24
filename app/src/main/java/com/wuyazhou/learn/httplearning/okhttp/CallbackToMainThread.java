5fed6c1394639a961ba9c060378220ac8f3c3604
wuyazhou <wuyazhou-ds@gome.com.cn>
2019-01-24 16:52:26 +0800

    添加注释

--- app/src/main/java/com/wuyazhou/learn/httplearning/okhttp/CallbackToMainThread.java
+++ app/src/main/java/com/wuyazhou/learn/httplearning/okhttp/CallbackToMainThread.java
@@ -13,6 +13,7 @@ import okhttp3.Response;
 /**
  * @author wuyzh
  * @fuction 将返回结果直接回调给UI线程，整个方法都在UI线程内执行
+ * CallbackToMainThread实现自Callback，Callback是OkHttp3请求回调要实现的接口
  * */
 public class CallbackToMainThread implements Callback {
     private Callback mCallback = null;
@@ -20,9 +21,15 @@ public class CallbackToMainThread implements Callback {
     private IOException mIOException = null;
     private String mStringResponse = null;
     private Bitmap mBitmapResponse = null;
+    /**
+     * @param callback 这里是请求结果的回调，其回调方法将会在UI线程内执行
+     * */
     public CallbackToMainThread(Callback callback){
         mCallback = callback;
     }
+    /**
+     * 此处new android.os.Handler(Looper.getMainLooper()).post(runnableFailureUI)是将失败通过UI线程返回
+     * */
     @Override
     public void onFailure(Call call, IOException e) {
         mCall = call;
@@ -30,6 +37,9 @@ public class CallbackToMainThread implements Callback {
         new android.os.Handler(Looper.getMainLooper()).post(runnableFailureUI);
     }
 
+    /**
+     * 这里会根据主线程内Callback的getTClassName实现来获取想要得到的数据类型，然后去做类型转化
+     * */
     @Override
     public void onResponse(Call call, Response response) {
         mCall = call;
@@ -72,6 +82,10 @@ public class CallbackToMainThread implements Callback {
         }
     };
 
+    /**
+     * 此处接口将会在UI线程内实现
+     * 成功失败都将会通过这里面的方法返回给UI线程
+     * */
     public interface Callback<T> {
         void onFailure(Call call, IOException e);
         void onResponse(Call call, T response) throws IOException;
--- app/src/main/java/com/wuyazhou/learn/httplearning/okhttp/OkHttpTest.java
+++ app/src/main/java/com/wuyazhou/learn/httplearning/okhttp/OkHttpTest.java
@@ -181,7 +181,5 @@ public class OkHttpTest {
                 return String.class.getName();
             }
         }));
-
-
     }
 }
